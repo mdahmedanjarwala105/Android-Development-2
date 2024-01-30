3151acc07f18b24b5fb3938d6bfe65b1bb8d1c51
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2024-01-31 00:15:08 +0530

    Fragments

--- app/src/main/java/com/maabook/maabook/MainActivity.kt
+++ app/src/main/java/com/maabook/maabook/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.FrameLayout
-import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toolbar: Toolbar
     private lateinit var frameLayout: FrameLayout
     private lateinit var navigationView: NavigationView
+    private var previousMenuItem: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,24 +33,47 @@ class MainActivity : AppCompatActivity() {
 
         setUpToolbar()
 
+        openDashboard()
+
         val actionBarDrawerToggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open_drawer, R.string.close_drawer)
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
+        onBackPressedDispatcher.addCallback(this, onBackInvokedCallback)
+
         navigationView.setNavigationItemSelectedListener {
+
+            if (previousMenuItem != null) {
+                previousMenuItem?.isChecked = false
+            }
+
+            it.isCheckable = true
+            it.isChecked = true
+            previousMenuItem = it
+
             when(it.itemId){
                 R.id.dashboard -> {
-                    Toast.makeText(this@MainActivity, "Dashboard", Toast.LENGTH_SHORT).show()
+                    openDashboard()
+                    drawerLayout.closeDrawers()
                 }
                 R.id.favourites -> {
-                    Toast.makeText(this@MainActivity, "Favourites", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame, FavouritesFragment()).commit()
+
+                    supportActionBar?.title = "Favourites"
+                    drawerLayout.closeDrawers()
                 }
                 R.id.profile -> {
-                    Toast.makeText(this@MainActivity, "Profile", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame, ProfileFragment()).commit()
+
+                    supportActionBar?.title = "Profile"
+                    drawerLayout.closeDrawers()
                 }
                 R.id.aboutApp -> {
-                    Toast.makeText(this@MainActivity, "About App", Toast.LENGTH_SHORT).show()
+                    supportFragmentManager.beginTransaction().replace(R.id.frame, AboutAppFragment()).commit()
+
+                    supportActionBar?.title = "About App"
+                    drawerLayout.closeDrawers()
                 }
             }
             return@setNavigationItemSelectedListener true
@@ -72,5 +96,22 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+    private fun openDashboard() {
+        val fragment = DashboardFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.commit()
+        supportActionBar?.title = "Dashboard"
+        navigationView.setCheckedItem(R.id.dashboard)
+    }
 
+    private val onBackInvokedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            when(supportFragmentManager.findFragmentById(R.id.frame)) {
+                !is DashboardFragment -> openDashboard()
+
+                else -> finish()
+            }
+        }
+    }
 }
